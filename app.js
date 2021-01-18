fd797cb1bf8cd7174090bc86a57dc84e69c6dd61
xuanngo10 <xuan.ngo10@gmail.com>
2021-01-17 23:47:18 -0800

    progress

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+// import { library, icons } from '@fortawesome/fontawesome-svg-core';
+// import {
+//   faHandScissors,
+//   faHandPaper,
+//   faHandRock,
+// } from '@fortawesome/free-solid-svg-icons';
+
+// library.add(faHandScissors, faHandPaper, faHandRock);
+
+const buttons = document.querySelectorAll('.button');
+const userInput = document.getElementById('user-input');
+const userDisplay = document.getElementById('user-display');
+
+function hideAnswers() {
+  const answer = document.querySelectorAll('.answer');
+
+  // Hide all answers first
+  for (let i = 0; i < answer.length; i++) {
+    if (!answer[i].classList.contains('hidden')) {
+      answer[i].classList.add('hidden');
+    }
+  }
+}
+
 function computerPlay() {
   let items = ['rock', 'paper', 'scissors'];
   let randomNum = Math.floor(Math.random() * Math.floor(3));
@@ -7,10 +31,11 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
+  const resultText = document.getElementById('result-text');
   playerSelection.toLowerCase();
 
   if (playerSelection === computerSelection) {
-    return 'Draw';
+    resultText.textContent = 'Draw';
   } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
     return 'You Lose! Scissors beat Paper';
   } else if (playerSelection === 'paper' && computerSelection === 'rock') {
@@ -37,4 +62,43 @@ function game() {
   }
 }
 
-game();
+// Display user's and opponents answer when button is clicked
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', e => {
+    let target = e.currentTarget.id;
+    const opponentAnswer = computerPlay();
+
+    // Hide answers first
+    hideAnswers();
+
+    // Display user's answer
+    if (target === 'rock') {
+      const userRock = document.getElementById('user-rock');
+      userRock.classList.remove('hidden');
+    } else if (target === 'paper') {
+      const userPaper = document.getElementById('user-paper');
+      userPaper.classList.remove('hidden');
+    } else if (target === 'scissors') {
+      const userScissors = document.getElementById('user-scissors');
+      userScissors.classList.remove('hidden');
+    } else {
+      console.log('error');
+    }
+    // Display opponent's answer
+    if (opponentAnswer === 'rock') {
+      const oppRock = document.getElementById('opponent-rock');
+      oppRock.classList.remove('hidden');
+    } else if (opponentAnswer === 'paper') {
+      const oppPaper = document.getElementById('opponent-paper');
+      oppPaper.classList.remove('hidden');
+    } else if (opponentAnswer === 'scissors') {
+      const oppScissors = document.getElementById('opponent-scissors');
+      oppScissors.classList.remove('hidden');
+    }
+
+    // Play the round
+    playRound(target, opponentAnswer);
+  });
+}
+
+// game();
